8c63901922577168f3ff78a0615191ac6bfd1a63
Davi Freitas <92342116+daviifreitas@users.noreply.github.com>
2022-11-26 18:22:56 -0300

    Refactor : Update connection String

--- Visudata.Infra.IoC/DependencyInjection.cs
+++ Visudata.Infra.IoC/DependencyInjection.cs
@@ -14,22 +14,21 @@ public static class DependencyInjection
 {
     public static void AddInfrastructure(this IServiceCollection services, ConfigurationManager config)
     {
-        //Local connection !!!
+
         services.AddDbContext<VisudataDbContext>(options => options.UseMySql(
-            "Server=localhost;Port=3306;Database=pi_db;User=root;Password=root;",
-            ServerVersion.Parse("8.0.30-mysql" , ServerType.MySql), m => m.MigrationsAssembly(typeof(VisudataDbContext).Assembly.FullName)));
+            "Server=recnplay-server.mysql.database.azure.com;UserID = davifmelo;Password=password13258046A@;Database=visudata_db;;",
+            ServerVersion.Parse("8.0.30-mysql", ServerType.MySql), m => m.MigrationsAssembly(typeof(VisudataDbContext).Assembly.FullName)));
 
         services.AddDbContext<VisudataDbContext>();
-        
+
         #region repositories 
-        
+
         services.AddScoped<IEnterpriseRepository, EnterpriseRepository>();
         services.AddScoped<ILogsRepository, LogsRepository>();
         services.AddScoped<IMachineRepository, MachineRepository>();
         services.AddScoped<IOutlierRegisterRepository, OutlierRegisterRepository>();
         services.AddScoped<IMachineCategoryRepository, MachineCategoryRepository>();
         services.AddScoped<IOutlierRegisterRepository, OutlierRegisterRepository>();
-        //services.AddScoped<IMachineCategoryRepository, CategoryRepository>();
         services.AddScoped<IUserSupportRepository, UserSupportRepository>();
         services.AddScoped<IEnterpriseStatusRepository, EnterpriseStatusRepository>();
         services.AddScoped<IUserProblemsCategoryRepository, UserProblemsCategoryRepository>();
