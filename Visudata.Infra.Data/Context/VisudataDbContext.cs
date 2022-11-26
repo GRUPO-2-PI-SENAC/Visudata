92b628b2072666c7a13bcf49c28916dccc9e4f9b
Davi Freitas <92342116+daviifreitas@users.noreply.github.com>
2022-11-26 18:22:42 -0300

    Refactor : Update seed data

--- Visudata.Infra.Data/Context/VisudataDbContext.cs
+++ Visudata.Infra.Data/Context/VisudataDbContext.cs
@@ -28,31 +28,37 @@ public class VisudataDbContext : DbContext
         modelBuilder.Entity<MachineCategory>().HasData(
             new MachineCategory
             {
+                Id = 1,
                 Created_at = DateTime.Now,
                 Name = "Injetoras",
             },
             new MachineCategory
             {
+                Id = 2,
                 Created_at = DateTime.Now,
                 Name = "Sensores"
             },
             new MachineCategory
             {
+                Id = 3,
                 Created_at = DateTime.Now,
                 Name = "Extratoras"
             },
             new MachineCategory
             {
+                Id = 4,
                 Created_at = DateTime.Now,
                 Name = "Empilhadeiras"
             },
             new MachineCategory
             {
+                Id = 5,
                 Created_at = DateTime.Now,
                 Name = "Esteiras"
             },
             new MachineCategory
             {
+                Id = 6,
                 Created_at = DateTime.Now,
                 Name = "Motores"
             }
@@ -61,31 +67,37 @@ public class VisudataDbContext : DbContext
         modelBuilder.Entity<UserProblemsCategory>().HasData(
             new UserProblemsCategory
             {
+                Id = 1,
                 Name = "Visualizaçäo de gráfico",
                 Created_at = DateTime.Now
             },
             new UserProblemsCategory
             {
+                Id = 2,
                 Name = "Exportaçäo de dados",
                 Created_at = DateTime.Now
             },
             new UserProblemsCategory
             {
+                Id = 3,
                 Name = "Cadastro de máquinas",
                 Created_at = DateTime.Now
             },
             new UserProblemsCategory
             {
+                Id = 4,
                 Name = "Ediçäo de máquinas",
                 Created_at = DateTime.Now
             },
             new UserProblemsCategory
             {
+                Id = 5,
                 Name = "Fechamento de programas",
                 Created_at = DateTime.Now
             },
             new UserProblemsCategory
             {
+                Id = 6,
                 Name = "Conexäo com sensores",
                 Created_at = DateTime.Now
             }
