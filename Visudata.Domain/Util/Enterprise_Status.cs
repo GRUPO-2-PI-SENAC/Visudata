9c93029aa39bda65fe51ce01301d382d01987ecd
Davi Freitas <92342116+daviifreitas@users.noreply.github.com>
2022-11-29 23:26:20 -0300

    add enterprise status as enum

---
+++ Visudata.Domain/Util/Enterprise_Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Domain.Util
+{
+    public enum Enterprise_Status
+    {
+        ACTIVE = 0 ,
+        DISABLE = 1 ,
+    }
+}
