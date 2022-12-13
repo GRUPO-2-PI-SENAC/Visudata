d3981418df9870021f8720ada66ba129e03cb03b
Davi Freitas <92342116+daviifreitas@users.noreply.github.com>
2022-12-13 02:59:13 -0300

    Ajustes

--- Visudata.API/Program.cs
+++ Visudata.API/Program.cs
@@ -3,13 +3,22 @@ using PI.Infra.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://localhost:55186",
+                                              "http://localhost:55187");
+                      });
+});
 
 var app = builder.Build();
 
@@ -20,6 +29,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
+//app.UseCors(builder =>
+//{
+//    builder
+//       .WithOrigins("https://localhost:55186", "http://localhost:55187")
+//       .SetIsOriginAllowedToAllowWildcardSubdomains()
+//       .AllowAnyHeader()
+//       .AllowCredentials()
+//       .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+//       .SetPreflightMaxAge(TimeSpan.FromSeconds(3600));
+//}
+//);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
