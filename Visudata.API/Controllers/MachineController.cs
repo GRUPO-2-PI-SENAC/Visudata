bfce8a8c2f35f5e11a12ec2dffedda65da2b5e06
Davi Freitas <davimello14@gmail.com>
2023-03-09 17:09:59 -0300

    Remove unecessary code

--- Visudata.API/Controllers/MachineController.cs
+++ Visudata.API/Controllers/MachineController.cs
@@ -42,52 +42,5 @@ namespace Visudata.API.Controllers
         }
 
 
-        //[HttpGet]
-        //[Route("[controller]/getall")]
-        //public async Task<IActionResult> GetAll()
-        //{
-        //    return Json((await _machineAppService.GetMachinesForApiList("76535764000143")).ToList());
-        //}
-
-        //[HttpPost]
-        //[Route("[controller]/add")]
-        //public async Task<IActionResult> Add([FromBody] AddMachineViewModel model)
-        //{
-        //    bool isAdded = await _machineAppService.Add(model, "76535764000143");
-
-        //    return isAdded ? Ok() : BadRequest("Error in create machine !");
-        //}
-
-        //[HttpGet]
-        //[Route("[controller]/{id}")]
-        //public async Task<IActionResult> GetById(int id)
-        //{
-        //    var machineForDetails = await _machineAppService.GetEditMachineModel(id);
-
-        //    return Json(machineForDetails);
-        //}
-
-        //[HttpPut]
-        //[Route("[controller]/update")]
-        //public async Task<IActionResult> Update(EditMachineViewModel model)
-        //{
-        //    bool isUpdated = await _machineAppService.UpdateMachine(model);
-
-        //    return isUpdated ? Ok("The machine are update with success!") : BadRequest("Some kind error occurred in update machine, conteact the adminsitrador");
-        //}
-
-        //[HttpDelete]
-        //[Route("[controller]/delete/{id}")]
-        //public async Task<IActionResult> Delete(int id)
-        //{
-        //    bool isRemoved = await _machineAppService.RemoveMachine(id);
-
-        //    return isRemoved ? Ok() : BadRequest("Some kind of error occurred");
-        //}
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
     }
 }
